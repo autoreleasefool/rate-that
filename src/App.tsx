62f06b7abed64e8e9f38fb2107035d16cc5e9008
Joseph Roque <github@josephroque.ca>
2021-01-14 22:11:50 -0500

    Update eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from '@shopify/restyle';
 import {RootNavigator} from 'navigation/RootNavigator';
-import {theme} from 'shared/theme';
 import {Box} from 'shared/components/Box';
+import {theme} from 'shared/theme';
 
 const StyledApp = () => {
   return (
